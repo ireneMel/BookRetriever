1c31f4a5dbeec7e10149f04308ddbcfc40e0fc4a
ireneMiller <iramelnykv@gmail.com>
2022-08-18 08:29:00 +0300

    added annotation for hilt

--- app/src/main/java/com/example/bookretriever/ui/MainActivity.kt
+++ app/src/main/java/com/example/bookretriever/ui/MainActivity.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bookretriever.BuildConfig
 import com.example.bookretriever.R
+import com.example.bookretriever.ui.fragments.MainFragment
 import com.example.bookretriever.ui.fragments.RegisterFragment
+import dagger.hilt.android.AndroidEntryPoint
 
 //String myApiKey = BuildConfig.API_KEY;
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       val a = BuildConfig.API_KEY
+//       val a = BuildConfig.API_KEY
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, RegisterFragment())
+            .replace(R.id.fragment_container, MainFragment())
             .commit()
 
     }
