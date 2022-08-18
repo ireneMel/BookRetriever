c3a0d2355c54e85db2c9e1cb4fadbc3b96e98d01
ireneMiller <iramelnykv@gmail.com>
2022-08-18 08:25:06 +0300

    dependency injection

---
+++ app/src/main/java/com/example/bookretriever/net/IBookClient.kt
@@ -0,0 +1,24 @@
+package com.example.bookretriever.net
+
+import com.example.bookretriever.models.json.BookResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface IBookClient {
+
+    @GET("/search/authors.json")
+    suspend fun getBooksByAuthor(
+        @Query("q") query: String,
+        @Query("mode") queryMode: String = "everything"
+    ): Response<BookResponse>
+
+    @GET("/search.json")
+    suspend fun getBookByTitle(
+        @Query("title") query: String,
+        @Query("mode") queryMode: String = "everything"
+    ): Response<BookResponse>
+
+    @GET("/trending/daily.json")
+    suspend fun getTrendingBooks(): Response<BookResponse>
+}
