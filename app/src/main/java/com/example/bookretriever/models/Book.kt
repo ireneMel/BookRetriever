ded68e1fba569a7bd56763afae4c4efc43447e32
ireneMiller <iramelnykv@gmail.com>
2022-08-18 08:26:15 +0300

    book

--- app/src/main/java/com/example/bookretriever/models/Book.kt
+++ app/src/main/java/com/example/bookretriever/models/Book.kt
@@ -1,22 +1,13 @@
 package com.example.bookretriever.models
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+//TODO make different fields in Book and UIBook
 
-//by default Covers API returns a blank image if the cover cannot be found.
-// If you append ?default=false to the end of the URL, then it returns a 404 instead
-
-@Entity(tableName = "books")
 data class Book(
     val isbnId: String?,
     var title: String? = null,
     var author: String? = null,
     val coverI: String? = null
 ) {
-
-    @PrimaryKey(autoGenerate = true)
-    var id: Int? = null
-
     // Get medium sized book cover from covers API
     val coverUrl: String
         get() = "https://covers.openlibrary.org/b/id/$coverI-M.jpg?default=false"
