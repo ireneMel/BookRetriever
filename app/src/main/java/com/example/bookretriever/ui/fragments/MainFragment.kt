a73c7300bd9fb5e9b0d60e768ffd497c6d9ff9a4
ireneMiller <iramelnykv@gmail.com>
2022-08-18 08:29:19 +0300

    added annotation for hilt

--- app/src/main/java/com/example/bookretriever/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/bookretriever/ui/fragments/MainFragment.kt
@@ -6,58 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.bookretriever.R
 import com.example.bookretriever.adapters.BookAdapter
 import com.example.bookretriever.databinding.FragmentMainBinding
-import com.example.bookretriever.models.Book
 import com.example.bookretriever.ui.viewmodels.MainViewModel
 import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
-//    private
-    private val viewModel: MainViewModel by viewModels() //{
-//        MainViewModel.Factory(
-//    }
+    private val viewModel: MainViewModel by viewModels()
 
-    //    private lateinit var client: BookClientInterface
     private lateinit var bookAdapter: BookAdapter
     private lateinit var binding: FragmentMainBinding
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
-    private var booksList: List<Book> = mutableListOf()
-
-//    private fun fetchBooks(query: String) {
-//        client = BookClientInterface.createClient()
-//        lifecycleScope.launch {
-////            val response = client.getBookByTitle(query, "everything")
-//            val response = client.getTrendingBooks()
-//            if (response.isSuccessful) {
-//                val bookResponse: BookResponse = response.body() ?: return@launch
-//                val list = bookResponse.works.map {
-//                    Book(
-//                        it?.isbn,
-////                        it.editionKey?.get(0)?.removePrefix("/works/"),
-//                        it?.title,
-//                        it.authorName?.get(0) ?: "not found",
-//                        it?.cover_i
-//                    )
-//                }
-//                bookAdapter.submitList(list)
-//            }
-//        }
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        viewModel = ViewModelProvider
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
@@ -76,8 +48,8 @@ class MainFragment : Fragment() {
             adapter = bookAdapter
         }
 
-        lifecycleScope.launch{
-            viewModel.list.onEach { bookAdapter.submitList(it) }.collect()
+        lifecycleScope.launch {
+            viewModel.uiBookList.onEach { bookAdapter.submitList(it) }.collect()
         }
 
 
