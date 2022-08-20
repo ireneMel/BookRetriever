50f1cc184fa7b0bd51dd810f7ce927c4216d1677
ireneMiller <iramelnykv@gmail.com>
2022-08-20 18:31:07 +0300

    changed schema

--- app/src/main/java/com/example/bookretriever/databases/BooksDatabase.kt
+++ app/src/main/java/com/example/bookretriever/databases/BooksDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 import com.example.bookretriever.databases.dao.BooksDao
 import com.example.bookretriever.models.BookEntity
 
-@Database(entities = [BookEntity::class], version = 1, exportSchema = false)
+@Database(entities = [BookEntity::class], version = 2, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class BooksDatabase : RoomDatabase() {
     abstract fun getDao(): BooksDao
