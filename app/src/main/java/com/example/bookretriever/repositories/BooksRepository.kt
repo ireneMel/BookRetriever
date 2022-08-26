dccff074bc7432dd7e18a75865946a3c7c9f23c9
ireneMiller <iramelnykv@gmail.com>
2022-08-26 13:21:17 +0200

    added deletion of books from database if isCacheExpired

--- app/src/main/java/com/example/bookretriever/repositories/BooksRepository.kt
+++ app/src/main/java/com/example/bookretriever/repositories/BooksRepository.kt
@@ -1,6 +1,6 @@
 package com.example.bookretriever.repositories
 
-import com.example.bookretriever.databases.dao.BooksDao
+import com.example.bookretriever.databases.trending.BooksDao
 import com.example.bookretriever.models.Book
 import com.example.bookretriever.models.BookEntity
 import com.example.bookretriever.net.IBookClient
@@ -10,13 +10,13 @@ import javax.inject.Inject
 
 class BooksRepository @Inject constructor(
     private val bookDao: BooksDao,
-    private val retrofit: IBookClient,
+    private val retrofit: IBookClient
 ) {
     suspend fun getTrendingBooks(): BookEntity? {
         val first: BookEntity? = bookDao.getTrendingBooks().first().firstOrNull()
         val isCacheExpired = System.currentTimeMillis() - (first?.timeInMillis ?: 0) > DELTA
-
         if (first == null || isCacheExpired) {
+            bookDao.deleteAll()
             //if there is no appropriate data in the database - the request to the server is sent
             //retrieved data has to be saved
             val response = retrofit.getTrendingBooks()//.body()
@@ -27,9 +27,12 @@ class BooksRepository @Inject constructor(
             val list = bookResponse.works.map {
                 Book(
                     it.isbn,
+                    listToString(it.authorName),
                     it.title,
-                    it.authorName?.get(0) ?: "unknown",
-                    it.cover_i
+                    2002,
+                    it.cover_i ?: "",
+                    false,
+                    ""
                 )
             }
             val finalEntity = BookEntity(System.currentTimeMillis(), list)
@@ -40,6 +43,15 @@ class BooksRepository @Inject constructor(
             //if database contains needed data - get and show it
             return first
         }
+    }
+
+    private fun listToString(authors: List<String>): String {
+        var res = ""
+        for (author in 0..authors.size - 2) {
+            res += "$author, "
+        }
+        res += authors[authors.size - 1]
+        return res
     }
 
 //    fun getBooksByTitleCached(title: String) = bookDao.getBookByTitle(title)
