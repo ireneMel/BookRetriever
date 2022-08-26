d6becb614d7e9b0c69852238fa0562b291b164f1
ireneMiller <iramelnykv@gmail.com>
2022-08-26 13:22:37 +0200

    deleted unused method

--- app/src/main/java/com/example/bookretriever/utils/GeneralFragment.kt
+++ app/src/main/java/com/example/bookretriever/utils/GeneralFragment.kt
@@ -3,6 +3,7 @@ package com.example.bookretriever.utils
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import androidx.navigation.NavController
 import com.example.bookretriever.R
 
 open class GeneralFragment : Fragment() {
@@ -12,11 +13,5 @@ open class GeneralFragment : Fragment() {
             msg,
             Toast.LENGTH_SHORT
         ).show()
-    }
-
-    fun goToFragment(fragment: Fragment) {
-        requireActivity().supportFragmentManager.commit {
-            replace(R.id.fragment_container, fragment)
-        }
     }
 }
