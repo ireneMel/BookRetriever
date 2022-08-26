24b049654eb5c767f3d72d6441547ad7652891f2
ireneMiller <iramelnykv@gmail.com>
2022-08-26 13:22:06 +0200

    renamed

--- app/src/main/java/com/example/bookretriever/databases/Converters.kt
+++ app/src/main/java/com/example/bookretriever/databases/Converters.kt
@@ -4,19 +4,17 @@ import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
 import com.example.bookretriever.models.Book
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 @ProvidedTypeConverter
 class Converters {
 
     @TypeConverter
-    fun bookToList(books: List<Book>): String {
+    fun listToString(books: List<Book>): String {
         return Gson().toJson(books)
     }
 
     @TypeConverter
-    fun listToBook(book: String): List<Book> {
+    fun stringToList(book: String): List<Book> {
         return Gson().fromJson(book, Array<Book>::class.java).toList()
     }
-
 }
