00163d294cac472e2e03e54d8bbd285b4556754b
ireneMiller <iramelnykv@gmail.com>
2022-08-26 13:22:52 +0200

    added navigation via bottom nav

--- app/src/main/java/com/example/bookretriever/ui/MainActivity.kt
+++ app/src/main/java/com/example/bookretriever/ui/MainActivity.kt
@@ -1,22 +1,44 @@
 package com.example.bookretriever.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.bookretriever.R
-import com.example.bookretriever.ui.fragments.authorization.LoginFragment
+import com.example.bookretriever.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
-//String myApiKey = BuildConfig.API_KEY;
+//TODO on long or double click add to the shelf
 
+//String myApiKey = BuildConfig.API_KEY;
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        supportFragmentManager.commit {
-            replace(R.id.fragment_container, LoginFragment())
+        setSupportActionBar(binding.toolbar)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.fragment_navigation_container)
+        navView.setupWithNavController(navController) //bottom navigation
+        navView.setOnItemReselectedListener { /* NO-OP */ }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.mainFragment,
+                R.id.shelfFragment,
+                R.id.profileFragment -> navView.visibility = View.VISIBLE
+                R.id.loginFragment,
+                R.id.registerFragment -> navView.visibility = View.GONE
+            }
         }
     }
+
 }
+
